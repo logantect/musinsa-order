ba7e6853a16e20802a6c256d6414dba6eb27b957
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-13 03:02:55 +0900

    refactor(refund): 유료 배송비 정책 전체환불 여부 체크 구현
    주문 무료배송 체크 메서드 구현
    전체환불 여부 체크 구현
    주문상품 ID equals and hashCode 구현

--- src/main/java/com/musinsa/orders/domain/refund/RefundPolicy.java
+++ src/main/java/com/musinsa/orders/domain/refund/RefundPolicy.java
@@ -2,12 +2,31 @@ package com.musinsa.orders.domain.refund;
 
 import com.musinsa.orders.domain.order.Money;
 import com.musinsa.orders.domain.order.Order;
+import com.musinsa.orders.domain.order.OrderLineItem;
 import java.util.List;
+import java.util.Objects;
 
 public class RefundPolicy {
 
   public Money calculateReturnShippingFee(Order order, List<Long> returnLineItemIds) {
+    List<OrderLineItem> returnLineItems = order.getOrderLineItems(returnLineItemIds);
+    boolean freeShippingFee = order.isFreeShippingFee();
+    boolean fullRefund = isFullRefund(order, returnLineItems);
+    if (freeShippingFee) {
+      if (fullRefund) {
+        return Money.from(5_000L);
+      } else {
+        return Money.from(2_500L);
+      }
+    }
     return Money.from(2_500L);
+  }
+
+  private boolean isFullRefund(Order order, List<OrderLineItem> returnLineItems) {
+    if (Objects.equals(order.orderLineItems(), returnLineItems)) {
+      return true;
+    }
+    return false;
   }
 
 }
