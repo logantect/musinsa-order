1ac89f8f846b0c8e4fb2d3196e7ed60ac9db705a
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-13 04:00:38 +0900

    test(refund): 무료 배송비 정책 주문 부분 환불시 반품비 2500원을 반환한다

--- src/test/java/com/musinsa/orders/domain/refund/RefundPolicyTest.java
+++ src/test/java/com/musinsa/orders/domain/refund/RefundPolicyTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.musinsa.orders.domain.order.Money;
 import com.musinsa.orders.domain.order.Order;
 import com.musinsa.orders.domain.refund.RefundPolicy;
+import com.musinsa.orders.infra.refund.InMemoryRefundRepository;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,10 +16,12 @@ import org.junit.jupiter.api.Test;
 class RefundPolicyTest {
 
   private RefundPolicy refundPolicy;
+  private RefundRepository refundRepository;
 
   @BeforeEach
   void setUp() {
-    refundPolicy = new RefundPolicy();
+    refundRepository = new InMemoryRefundRepository();
+    refundPolicy = new RefundPolicy(refundRepository);
   }
 
   @Test
@@ -44,6 +47,19 @@ class RefundPolicyTest {
     ));
 
     Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(1L, 2L, 3L));
+    assertThat(actual).isEqualTo(Money.from(2_500L));
+  }
+
+  @Test
+  @DisplayName("무료 배송비 정책 주문 부분 환불시 반품비 2500원을 반환한다")
+  void calculateReturnShippingFee_FreeShippingFeePartialRefund() {
+    Order order = createOrder(1L, List.of(
+        createOrderLineItem(1L, 1L, "셔츠A", 40_000L),
+        createOrderLineItem(2L, 2L, "셔츠B", 50_000L),
+        createOrderLineItem(3L, 3L, "셔츠C", 60_000L)
+    ));
+
+    Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(2L));
     assertThat(actual).isEqualTo(Money.from(2_500L));
   }
 }
