289c296859b9f4be46ed764608063eb24a128107
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-14 20:07:15 +0900

    test(refund): @Nested 어노테이션 적용하여 가독성 처리

--- src/test/java/com/musinsa/orders/domain/refund/RefundPolicyTest.java
+++ src/test/java/com/musinsa/orders/domain/refund/RefundPolicyTest.java
@@ -11,6 +11,7 @@ import com.musinsa.orders.infra.refund.InMemoryRefundRepository;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class RefundPolicyTest {
@@ -24,55 +25,66 @@ class RefundPolicyTest {
     refundPolicy = new RefundPolicy(refundRepository);
   }
 
-  @Test
-  @DisplayName("유료 배송비 정책 주문 부분 환불시 반품비 2500원을 반환한다")
-  void calculateReturnShippingFee() {
-    Order order = createOrder(1L, List.of(
-        createOrderLineItem(1L, 1L, "신발A", 15_000L),
-        createOrderLineItem(2L, 2L, "신발B", 16_000L),
-        createOrderLineItem(3L, 3L, "신발C", 17_000L)
-    ));
+  @Nested
+  @DisplayName("유료 배송비 정책 주문")
+  class ShippingFee {
 
-    Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(2L));
-    assertThat(actual).isEqualTo(Money.from(2_500L));
+    @Test
+    @DisplayName("부분 환불시 반품비 2500원을 반환한다")
+    void calculateReturnShippingFee() {
+      Order order = createOrder(1L, List.of(
+          createOrderLineItem(1L, 1L, "신발A", 15_000L),
+          createOrderLineItem(2L, 2L, "신발B", 16_000L),
+          createOrderLineItem(3L, 3L, "신발C", 17_000L)
+      ));
+
+      Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(2L));
+      assertThat(actual).isEqualTo(Money.from(2_500L));
+    }
+
+    @Test
+    @DisplayName("전체 환불시 반품비 2500원을 반환한다")
+    void calculateReturnShippingFee_FullRefund() {
+      Order order = createOrder(1L, List.of(
+          createOrderLineItem(1L, 1L, "신발A", 15_000L),
+          createOrderLineItem(2L, 2L, "신발B", 16_000L),
+          createOrderLineItem(3L, 3L, "신발C", 17_000L)
+      ));
+
+      Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(1L, 2L, 3L));
+      assertThat(actual).isEqualTo(Money.from(2_500L));
+    }
   }
 
-  @Test
-  @DisplayName("유료 배송비 정책 주문 전체 환불시 반품비 2500원을 반환한다")
-  void calculateReturnShippingFee_FullRefund() {
-    Order order = createOrder(1L, List.of(
-        createOrderLineItem(1L, 1L, "신발A", 15_000L),
-        createOrderLineItem(2L, 2L, "신발B", 16_000L),
-        createOrderLineItem(3L, 3L, "신발C", 17_000L)
-    ));
+  @Nested
+  @DisplayName("무료 배송비 정책 주문")
+  class FreeShippingFee {
 
-    Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(1L, 2L, 3L));
-    assertThat(actual).isEqualTo(Money.from(2_500L));
+    @Test
+    @DisplayName("부분 환불시 반품비 2500원을 반환한다")
+    void calculateReturnShippingFee_FreeShippingFeePartialRefund() {
+      Order order = createOrder(1L, List.of(
+          createOrderLineItem(1L, 1L, "셔츠A", 40_000L),
+          createOrderLineItem(2L, 2L, "셔츠B", 50_000L),
+          createOrderLineItem(3L, 3L, "셔츠C", 60_000L)
+      ));
+
+      Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(2L));
+      assertThat(actual).isEqualTo(Money.from(2_500L));
+    }
+
+    @Test
+    @DisplayName("전체 환불시 반품비 5000원을 반환한다")
+    void calculateReturnShippingFee_FreeShippingFeeFullRefund() {
+      Order order = createOrder(1L, List.of(
+          createOrderLineItem(1L, 1L, "셔츠A", 40_000L),
+          createOrderLineItem(2L, 2L, "셔츠B", 50_000L),
+          createOrderLineItem(3L, 3L, "셔츠C", 60_000L)
+      ));
+
+      Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(1L, 2L, 3L));
+      assertThat(actual).isEqualTo(Money.from(5_000L));
+    }
   }
 
-  @Test
-  @DisplayName("무료 배송비 정책 주문 부분 환불시 반품비 2500원을 반환한다")
-  void calculateReturnShippingFee_FreeShippingFeePartialRefund() {
-    Order order = createOrder(1L, List.of(
-        createOrderLineItem(1L, 1L, "셔츠A", 40_000L),
-        createOrderLineItem(2L, 2L, "셔츠B", 50_000L),
-        createOrderLineItem(3L, 3L, "셔츠C", 60_000L)
-    ));
-
-    Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(2L));
-    assertThat(actual).isEqualTo(Money.from(2_500L));
-  }
-
-  @Test
-  @DisplayName("무료 배송비 정책 주문 부분 환불시 반품비 5000원을 반환한다")
-  void calculateReturnShippingFee_FreeShippingFeeFullRefund() {
-    Order order = createOrder(1L, List.of(
-        createOrderLineItem(1L, 1L, "셔츠A", 40_000L),
-        createOrderLineItem(2L, 2L, "셔츠B", 50_000L),
-        createOrderLineItem(3L, 3L, "셔츠C", 60_000L)
-    ));
-
-    Money actual = refundPolicy.calculateReturnShippingFee(order, List.of(1L, 2L, 3L));
-    assertThat(actual).isEqualTo(Money.from(5_000L));
-  }
 }
