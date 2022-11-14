41c74be53905489e8cb10522563ca6cc5fabd995
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-14 22:26:05 +0900

    test(exchange): @Nested 어노테이션 적용

--- src/test/java/com/musinsa/orders/domain/exchange/ExchangePolicyTest.java
+++ src/test/java/com/musinsa/orders/domain/exchange/ExchangePolicyTest.java
@@ -10,6 +10,7 @@ import com.musinsa.orders.domain.order.Order;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ExchangePolicyTest {
@@ -21,30 +22,53 @@ class ExchangePolicyTest {
     exchangePolicy = new ExchangePolicy();
   }
 
-  @Test
-  @DisplayName("주문 상품 교환 반품비 계산 요청에 왕복 반품비 5000원을 반환한다")
-  void calculateReturnShippingFee() {
-    Order order = createOrder(1L, List.of(
-        createOrderLineItem(1L, 1L, "신발A", 15_000L),
-        createOrderLineItem(2L, 2L, "신발B", 16_000L),
-        createOrderLineItem(3L, 3L, "신발C", 17_000L)
-    ));
+  @Nested
+  @DisplayName("주문 상품")
+  class OrderLineItem {
+    @Test
+    @DisplayName("이 존재하지 않으면 반품비를 계산할 수 없다")
+    void calculateReturnShippingFee_NotExistLineItem() {
+      Order order = createOrder(1L, List.of(
+          createOrderLineItem(1L, 1L, "신발A", 15_000L),
+          createOrderLineItem(2L, 2L, "신발B", 16_000L),
+          createOrderLineItem(3L, 3L, "신발C", 17_000L)
+      ));
 
-    Money actual = exchangePolicy.calculateReturnShippingFee(order, List.of(1L));
-    assertThat(actual).isEqualTo(Money.from(5_000L));
+      assertThatIllegalArgumentException()
+          .isThrownBy(() -> exchangePolicy.calculateReturnShippingFee(order, List.of(4L)));
+    }
   }
 
-  @Test
-  @DisplayName("주문 상품이 존재하지 않으면 반품비를 계산할 수 없다")
-  void calculateReturnShippingFee_NotExistLineItem() {
-    Order order = createOrder(1L, List.of(
-        createOrderLineItem(1L, 1L, "신발A", 15_000L),
-        createOrderLineItem(2L, 2L, "신발B", 16_000L),
-        createOrderLineItem(3L, 3L, "신발C", 17_000L)
-    ));
+  @Nested
+  @DisplayName("유료 배송비 정책 주문")
+  class ShippingFee {
 
-    assertThatIllegalArgumentException()
-        .isThrownBy(() -> exchangePolicy.calculateReturnShippingFee(order, List.of(4L)));
+    @Test
+    @DisplayName("부분 교환시 반품비 5000원을 반환한다")
+    void calculateReturnShippingFee() {
+      Order order = createOrder(1L, List.of(
+          createOrderLineItem(1L, 1L, "신발A", 15_000L),
+          createOrderLineItem(2L, 2L, "신발B", 16_000L),
+          createOrderLineItem(3L, 3L, "신발C", 17_000L)
+      ));
+
+      Money actual = exchangePolicy.calculateReturnShippingFee(order, List.of(1L));
+      assertThat(actual).isEqualTo(Money.from(5_000L));
+    }
+
+    @Test
+    @DisplayName("전체 교환시 반품비 5000원을 반환한다")
+    void calculateReturnShippingFee_Full() {
+      Order order = createOrder(1L, List.of(
+          createOrderLineItem(1L, 1L, "신발A", 15_000L),
+          createOrderLineItem(2L, 2L, "신발B", 16_000L),
+          createOrderLineItem(3L, 3L, "신발C", 17_000L)
+      ));
+
+      Money actual = exchangePolicy.calculateReturnShippingFee(order, List.of(1L, 2L, 3L));
+      assertThat(actual).isEqualTo(Money.from(5_000L));
+    }
+
   }
 
 }
