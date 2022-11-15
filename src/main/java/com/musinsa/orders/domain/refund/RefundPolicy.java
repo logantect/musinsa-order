6241a4a0f208be1a29d9554b634d8559eaf33018
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-15 16:12:43 +0900

    refactor(refund): 환불 상품 일급컬렉션 적용 테스트 실패 개선

--- src/main/java/com/musinsa/orders/domain/refund/RefundPolicy.java
+++ src/main/java/com/musinsa/orders/domain/refund/RefundPolicy.java
@@ -9,8 +9,8 @@ import com.musinsa.orders.domain.order.ShippingFeePolicy;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.ObjectUtils;
 
 @RequiredArgsConstructor
 public class RefundPolicy {
@@ -18,14 +18,15 @@ public class RefundPolicy {
   private final RefundRepository refundRepository;
 
   public Money calculateReturnShippingFee(Order order, List<Long> returnLineItemIds) {
-    List<Long> refundedLineItemIds = getRefundedLineItemIds(order.id());
+    RefundLineItems refundRequestLineItems = RefundLineItems.from(returnLineItemIds);
+    List<RefundLineItem> refundedLineItems = getRefundedLineItems(order.id());
+
     validateNotExist(order, returnLineItemIds);
-    validateAlreadyRefund(refundedLineItemIds, returnLineItemIds);
+    validateAlreadyRefund(refundRequestLineItems, refundedLineItems);
 
-    boolean freeShippingFee = order.isFreeShippingFee();
-    boolean fullRefund = isFullRefund(order, concat(refundedLineItemIds, returnLineItemIds));
+    boolean fullRefund = isFullRefund(order, refundRequestLineItems.concat(refundedLineItems));
 
-    if (freeShippingFee && fullRefund) {
+    if (order.isFreeShippingFee() && fullRefund) {
       return ROUND_TRIP_SHIPPING_FEE;
     }
     return ShippingFeePolicy.SHIPPING_FEE;
@@ -38,31 +39,28 @@ public class RefundPolicy {
     }
   }
 
-  private static void validateAlreadyRefund(List<Long> refundedLineItemIds,
-      List<Long> returnLineItemIds) {
-    refundedLineItemIds.forEach(it -> {
-      if (returnLineItemIds.contains(it)) {
-        throw new IllegalArgumentException("이미 환불진행 중이거나 환불완료된 주문 상품입니다. 주문 상품 ID: " + it);
-      }
-    });
+  private void validateAlreadyRefund(
+      RefundLineItems refundRequestLineItems,
+      List<RefundLineItem> refundedLineItems
+  ) {
+    if (ObjectUtils.isEmpty(refundedLineItems)) {
+      return;
+    }
+
+    if (refundRequestLineItems.existRefundLineItem(refundedLineItems)) {
+      throw new IllegalArgumentException("이미 환불진행 중이거나 환불완료된 주문 상품이 존재합니다");
+    }
   }
 
-  private boolean isFullRefund(Order order, List<Long> returnLineItemIds) {
-    return Objects.equals(order.orderLineItems(), order.getOrderLineItems(returnLineItemIds));
+  private boolean isFullRefund(Order order, RefundLineItems refundLineItems) {
+    return Objects.equals(order.orderLineItems(),
+        order.getOrderLineItems(refundLineItems.getOrderLineItemIds()));
   }
 
-  private List<Long> concat(List<Long> refundedLineItemIds, List<Long> returnLineItemIds) {
-    return Stream.concat(
-        refundedLineItemIds.stream(),
-        returnLineItemIds.stream()
-    ).toList();
-  }
-
-  private List<Long> getRefundedLineItemIds(final Long orderId) {
+  private List<RefundLineItem> getRefundedLineItems(final Long orderId) {
     List<Refund> refundedLineItems = refundRepository.findByOrderId(orderId);
     return refundedLineItems.stream()
         .flatMap(refund -> refund.refundLineItems().stream())
-        .map(RefundLineItem::orderLineItemId)
         .collect(Collectors.toList());
   }
 
