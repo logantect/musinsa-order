59abbb11d3bf32301a9d3f0b80bd28fb5ce264c8
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-15 17:11:32 +0900

    refactor(refund): RefundRequestLineItems 값 객체 구현
    환불 규정 도메인 서비스 리팩토링
    - RefundRequestLineItems 도입
    - Order 도메인 환불 상품 존재 여부
    체크 메서드 추가

---
+++ src/main/java/com/musinsa/orders/domain/refund/RefundRequestLineItems.java
@@ -0,0 +1,31 @@
+package com.musinsa.orders.domain.refund;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
+
+import com.musinsa.orders.domain.order.Order;
+import java.util.List;
+
+public record RefundRequestLineItems(
+    RefundLineItems refundRequestLineItems,
+    List<RefundLineItem> refundedLineItems
+) {
+
+  public RefundRequestLineItems {
+    if (!isEmpty(refundedLineItems) && refundRequestLineItems.existRefundLineItem(refundedLineItems)) {
+      throw new IllegalArgumentException("이미 환불진행 중이거나 환불완료된 주문 상품이 존재합니다");
+    }
+  }
+
+  public RefundRequestLineItems(
+      List<Long> refundRequestLineItemIds,
+      List<RefundLineItem> refundedLineItems
+  ) {
+    this(RefundLineItems.from(refundRequestLineItemIds), refundedLineItems);
+  }
+
+  public boolean isFullRefund(Order order) {
+    RefundLineItems refundLineItems = refundRequestLineItems.concat(refundedLineItems);
+    return order.allMatchLineItems(refundLineItems.getOrderLineItemIds());
+  }
+
+}
